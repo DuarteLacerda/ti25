3bb0d28d207191fa848236ac5e1718399a4a0f2b
Duarte de Abreu Sousa Lacerda <duartelacerda4@gmail.com>
2025-03-30 13:10:26 +0100

    30/03/2025

--- ti/add-ons/footer.php
+++ ti/add-ons/footer.php
@@ -1,8 +1,7 @@
 <footer class="bg-body-tertiary">
     <div class="d-flex justify-content-between align-items-center">
         <div class="text-center p-3 ms-5">
-            <a href="https://duartelacerda.github.io" target="_blank"><img src="assets/imagens/signed.png"
-                    alt="Duarte Lacerda" class="img-fluid" width="150"></a>
+            <a href="https://duartelacerda.github.io" target="_blank"><img src="assets/imagens/ei.png" alt="Duarte Lacerda" class="img-fluid" width="200"></a>
         </div>
         <div class="text-center p-3 me-5">
             <a href="https://www.ipleiria.pt/curso/licenciatura-em-engenharia-informatica/" target="_blank"><img src="assets/imagens/estg.png" alt="ESTG" width="150"></a>
