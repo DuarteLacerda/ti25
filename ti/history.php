c3e1788b9c86ec8c363a2621874708e0b0ea8a7c
Duarte de Abreu Sousa Lacerda <duartelacerda4@gmail.com>
2025-04-09 19:20:58 +0100

    More fixes - 09/04/25

--- ti/history.php
+++ ti/history.php
@@ -185,6 +185,7 @@ $historico = array_slice($historico, 0, 50); // Limitar a 50 entradas
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+    <script src="assets/script.js"></script>
 </body>
 
 </html>
